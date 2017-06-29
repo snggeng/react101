fa4775e87105867283ec2fc1909fc6471cc9c84f
Geng <snggeng@gmail.com>
2017-06-29 14:24:39 +0800

    done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,25 @@
 // Import modules
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 // Import components
-import Header from '../Header/Header';
-import Cart from '../Cart/Cart';
-import Footer from '../Footer/Footer';
+import Header from '../Header/Header'
+import Cart from '../Cart/Cart'
+import Footer from '../Footer/Footer'
 
 // Import static assets
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App container">
-        <Header title="My amazing cart"/>
-        <Cart/>
-        <Footer/>
-
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App container">
+				<Header title="My amazing cart" />
+				<Cart />
+				<Footer />
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/components/Cart/Create/Create.js
@@ -0,0 +1,86 @@
+import React, { Component, PropTypes } from 'react'
+import uuid from 'uuid'
+/**
+ * Create
+ */
+class Create extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      price: 0,
+      id: uuid.v4()
+    }
+  }
+
+  onChange = (e) => {
+    let state = this.state
+
+    if (e.target.name == 'name') {
+      state.title = e.target.value
+    }
+
+    if (e.target.name == 'price') {
+      state.price = e.target.value
+    }
+
+    this.setState(state)
+  }
+
+  onClick = e => {
+    this.props.createProduct(this.state)
+
+    this.setState({
+      title: '',
+      price: 0,
+      id: uuid.v4()
+    })
+  }
+
+  render() {
+    return (
+      <section className="row create">
+        <div className="col-md-2 name">
+          <div className="form-group">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="form-control"
+              onChange={this.onChange}
+            />
+          </div>
+        </div>
+        <div className="col-md-2 price">
+          <div className="form-group">
+            <input
+              type="number"
+              name="price"
+              min={0}
+              placeholder="Price"
+              className="form-control"
+              onChange={this.onChange}
+            />
+          </div>
+        </div>
+        <div className="col-md-2 createProduct">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={this.onClick}
+          >
+            Create
+          </button>
+        </div>
+        <div className="col-md-2 col-md-offset-4 calc">
+          <button type="button" className="btn btn-info">
+            Calculate
+          </button>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Create
